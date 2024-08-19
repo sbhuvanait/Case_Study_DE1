9175796e1c35c6c79fbe244f9925098a9cc04560
sbhuvanait <40162293+sbhuvanait@users.noreply.github.com>
2024-08-19 19:35:36 +0530

    Added unit test case
    A

--- src/main.py
+++ src/main.py
@@ -6,44 +6,30 @@ from pyspark.sql.functions import year, month, to_date, regexp_replace, sum
 # returnFile = './dataset/Global Superstore Sales - Global Superstore Returns.csv'
 
 def main(salesFile, returnFile):
-    spark = SparkSession.builder.appName("case_study_1").master("local[*]").getOrCreate()
-    spark.conf.set("spark.sql.parquet.int96RebaseModeInWrite", "CORRECTED")
-    spark.conf.set("spark.sql.legacy.timeParserPolicy", "LEGACY")
-
+    spark = SparkSession.builder.appName("spark_pgm").master("local[*]").getOrCreate()
     dfSales :DataFrame = spark.read.option("header", "true").option("inferSchema", "true").csv(salesFile)
     #dfSales.show()
 
     dfReturn :DataFrame = spark.read.option("header", "true").option("inferSchema", "true").csv(returnFile)
     #dfReturn.show()
-
-    # TODO: For the total profit and total quantity calculations the returns data should be used to exclude all returns
-    # Filter out the the data matching in retuns for Sale file
     dfCombinedSales :DataFrame = dfSales.join(dfReturn, ["Order ID"], "leftanti")
 
-    # Casting fields
-    dfFormattedSales = dfCombinedSales\
-                        .withColumn("Order Date", to_date(dfCombinedSales["Order Date"], "MM/dd/yyyy"))\
-                        .withColumn("Profit", regexp_replace("Profit", "\\$", ""))\
-                        .withColumn("Profit", regexp_replace("Profit", ",", "").cast("float"))
+    dfFormattedSales = dfCombinedSales.withColumn("Order Date", to_date(dfCombinedSales["Order Date"], "MM/dd/yyyy")).withColumn("Profit", regexp_replace("Profit", "[\$,]", "")).cast("float"))
+    # Create year and month                   
+    dfFormattedSales = dfFormattedSales.withColumn("Year", year(dfFormattedSales["Order Date"])).withColumn("Month", month(dfFormattedSales["Order Date"]))
+    # aggregate (sum) Profit and Quantity, based on Year, Month, Category, Sub Category
+    dfFormattedSales = dfFormattedSales.groupBy(["Year", "Month", "Category", "Sub-Category"]).agg(sum("Profit").alias("Total Profit"),sum("Quantity").alias("Total Quantity Sold"))
+    # Store the data in a partition based on year month
+    dfFormattedSales.write.partitionBy(["Year", "Month"]).mode("overwrite").format("csv").option("header", "true").save("./dataset/case_study_report.csv")
 
-    # Creating year and month fields                   
-    dfFormattedSales = dfFormattedSales.withColumn("Year", year(dfFormattedSales["Order Date"]))\
-                        .withColumn("Month", month(dfFormattedSales["Order Date"]))
-    
-    # TODO: That contains the aggregate (sum) Profit and Quantity, based on Year, Month, Category, Sub Category
-    dfFormattedSales = dfFormattedSales.groupBy(["Year", "Month", "Category", "Sub-Category"])\
-                                        .agg(sum("Profit").alias("Total Profit"),
-                                            sum("Quantity").alias("Total Quantity Sold"))
-    
-    # TODO: This data has to be stored in a partition based on year month. like year=2014/month=11
-    dfFormattedSales.coalesce(1)\
-        .write.partitionBy(["Year", "Month"]).mode("overwrite").format("csv")\
-        .option("header", "true")\
-        .save("./dataset/case_study_report.csv")\
+# unit test case
+def check_join_count (df1: int, df2: int):
+    """ Testing the join operation """
+    assert df1==df2
+check_join_count(dfSales.count(), dfFormattedSales.count())
 
 
-# TODO: UNIT TEST
 if __name__ == '__main__':
-    # TODO: Inputs should be sales and return files
+    # Sales and retun as input
     _, salesFile, returnFile = argv
     main(salesFile, returnFile)  
